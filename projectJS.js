8e5667c3f4f051003a8297411008f55ddf09665d
Andre Sapp <andresapp93@gmail.com>
2023-02-05 20:04:32 -0500

    Add files via upload

---
+++ projectJS.js
@@ -0,0 +1,15 @@
+function hover(img){
+ img.src = "gokurose.png"
+};
+
+function hoverOut(img){
+    img.src = "gokublue.png"
+   }
+   function hover2(img){
+    img.src = "lightningBlade.png"
+   };
+   
+   function hoverOut2(img){
+       img.src = "kakashiReading.png"
+      }
+   
